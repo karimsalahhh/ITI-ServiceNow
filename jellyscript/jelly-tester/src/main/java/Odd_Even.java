cfed5239dcce80912374ec1fe533e9ce57e4e7b7
Karim Salah <kareemsalahahmed11@gmail.com>
2026-02-27 18:42:22 +0200

    Add jelly tester project

---
+++ jellyscript/jelly-tester/src/main/java/Odd_Even.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Odd_Even {
+    public String oddEven(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
---
+++ jellyscript/jelly-tester/src/main/java/SimpleFunction.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class SimpleFunction {  
+  public String hello(String name){
+    return "Hello " + name + "!";
+  }
+    
+}
